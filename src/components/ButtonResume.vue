4121f6494459f8a009473c121636bec90049ec26
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2023-03-29 10:27:51 -0500

    Update ButtonResume.vue

--- src/components/ButtonResume.vue
+++ src/components/ButtonResume.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :class="`text-${textColor} border-${outlineColor}`" class="border rounded-xl font-semibold py-2 px-5 text-button transition duration-300 ease-in-out" href="../aydenwyer_resume.pdf" download="AydenWyer_Resume">Resume</a>
+    <a :class="`text-${textColor} border-${outlineColor}`" class="border border-primary rounded-xl font-semibold py-2 px-5 text-button transition duration-300 ease-in-out" href="../aydenwyer_resume.pdf" download="AydenWyer_Resume">Resume</a>
 </template>
 
 <script>
