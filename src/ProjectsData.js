6691731c2c300e1e0d2c1478477e621ef66dd119
Ayden Wyer <102749343+aydenwyer@users.noreply.github.com>
2023-03-29 14:49:45 -0500

    Update ProjectsData.js

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -22,13 +22,5 @@ export default [
         "codeLink": "https://github.com/aydenwyer/tip-calculator-app",
         "previewLink": "https://aydenwyer.github.io/tip-calculator-app/",
         "image": "../tax-calculator_app.png"
-    },
-    {
-        "name": "This project sucked",
-        "stack": ["HTML", "CSS", "JavaScript"],
-        "description": "Visualize your CSS box shadow with easy to use sliders and color inputs. After you’re happy with the result, export the code and use it in your current project!",
-        "codeLink": "#",
-        "previewLink": "#",
-        "image": "url"
     }
 ]
